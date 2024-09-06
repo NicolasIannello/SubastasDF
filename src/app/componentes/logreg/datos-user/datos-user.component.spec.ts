725aec74a157523779501456b9c1e97a18d7999e
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-06 14:57:07 -0300

    datos user 1ra

---
+++ src/app/componentes/logreg/datos-user/datos-user.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DatosUserComponent } from './datos-user.component';
+
+describe('DatosUserComponent', () => {
+  let component: DatosUserComponent;
+  let fixture: ComponentFixture<DatosUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [DatosUserComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(DatosUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/componentes/logreg/datos-user/datos-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './datos-user.component.html',
+  styleUrl: './datos-user.component.css'
+})
+export class DatosUserComponent {
+
+}
