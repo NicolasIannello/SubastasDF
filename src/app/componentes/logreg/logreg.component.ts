ebf9459e7623aad7afaf7ee6a22d1d59d34af2f3
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-07 18:42:57 -0300

    cambier tipo user

--- src/app/componentes/logreg/logreg.component.ts
+++ src/app/componentes/logreg/logreg.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { DatosUserComponent } from "./datos-user/datos-user.component";
+import { CommonModule } from '@angular/common';
+import { DatosAccesoComponent } from "./things/datos-acceso/datos-acceso.component";
 
 @Component({
   selector: 'app-logreg',
   standalone: true,
-  imports: [DatosUserComponent],
+  imports: [DatosUserComponent, CommonModule, DatosAccesoComponent],
   templateUrl: './logreg.component.html',
   styleUrl: './logreg.component.css'
 })
 export class LogregComponent {
+  tipo:string="user";
 
+  cambiarTipo(tipo:string){
+    this.tipo=tipo;
+  }
 }
