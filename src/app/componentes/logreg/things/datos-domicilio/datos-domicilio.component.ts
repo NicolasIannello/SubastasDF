1bf367dc8e748d72f38a93ac736323a3d2b83469
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-07 18:44:09 -0300

    campos domicilio

---
+++ src/app/componentes/logreg/things/datos-domicilio/datos-domicilio.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Countries } from '../../datos-user/paises';
+import { countries } from '../../datos-user/paises-data';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-datos-domicilio',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './datos-domicilio.component.html',
+  styleUrl: './datos-domicilio.component.css'
+})
+export class DatosDomicilioComponent {
+  Paises:Countries[]=countries;
+  campos:Array<any>=[1000,1000,'','',''];
+  @Input() alertasDom:Array<string>=[];
+  @Output() messageEvent = new EventEmitter<Array<any>>();
+
+  ngOnInit(): void {
+    this.sendDatosDom();
+  }
+
+  sendDatosDom() {
+    this.messageEvent.emit(this.campos,);
+  }
+}
