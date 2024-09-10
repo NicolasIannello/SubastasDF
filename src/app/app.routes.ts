1b0bde800d8f18e1aeb1f55b6add6aa942e052e1
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-10 19:51:38 -0300

    ruta admin / childrens

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LogregComponent } from './componentes/logreg/logreg.component';
 import { VerificacionComponent } from './componentes/verificacion/verificacion.component';
+import { LandingComponent } from './componentes/landing/landing.component';
+import { PanelAdminComponent } from './componentes/panel-admin/panel-admin.component';
 
 export const routes: Routes = [
-    { path: '', component: InicioComponent },
-    { path: 'login', component: LogregComponent },
-    { path: 'verificar/:token', component: VerificacionComponent },
+    { path: '', component: LandingComponent , children: [
+        { path: '', component: InicioComponent },
+        { path: 'login', component: LogregComponent },
+        { path: 'verificar/:token', component: VerificacionComponent },
+    ]},
+    { path: 'panelAdmin', component: PanelAdminComponent , children: [
+        { path: '**',   redirectTo: '', pathMatch: 'full' },
+    ]},
     { path: '**',   redirectTo: '', pathMatch: 'full' },
 ];
