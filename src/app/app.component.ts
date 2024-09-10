070b797d8011a74e59398b36630695ab865a0f5c
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-10 19:50:23 -0300

    moved to landing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component, HostListener, inject, Injector, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./componentes/navbar/navbar.component";
 import { FooterComponent } from "./componentes/footer/footer.component";
@@ -10,18 +10,6 @@ import { FooterComponent } from "./componentes/footer/footer.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-  injector = inject(Injector);
-  title = 'subastas';
-  width:number | undefined;
-  cap:number = 1230;
-
-  ngOnInit() {
-    afterNextRender(() => this.width=window.innerWidth, {injector: this.injector});
-  }
-
-  @HostListener('window:resize', ['$event'])
-  onResize(_event: any) {
-    this.width=window.innerWidth;
-  }
+export class AppComponent{
+  
 }
