afadf2f6b15078168d5db83aab9789c0a0f0a2dd
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-10 19:51:19 -0300

    panel admin 1st

---
+++ src/app/componentes/panel-admin/panel-admin.component.ts
@@ -0,0 +1,25 @@
+import { afterNextRender, Component, HostListener, inject, Injector } from '@angular/core';
+import { AzulComponent } from "../navbar/azul/azul.component";
+
+@Component({
+  selector: 'app-panel-admin',
+  standalone: true,
+  imports: [AzulComponent],
+  templateUrl: './panel-admin.component.html',
+  styleUrl: './panel-admin.component.css'
+})
+export class PanelAdminComponent {
+  injector = inject(Injector);
+  title = 'subastas';
+  width:number | undefined;
+  cap:number = 1230;
+
+  ngOnInit() {
+    afterNextRender(() => this.width=window.innerWidth, {injector: this.injector});
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(_event: any) {
+    this.width=window.innerWidth;
+  }
+}
