f2aec62ff360d3a86980aa3623f74558bee851f8
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-10 15:12:31 -0300

    verificar mail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './componentes/inicio/inicio.component';
 import { LogregComponent } from './componentes/logreg/logreg.component';
+import { VerificacionComponent } from './componentes/verificacion/verificacion.component';
 
 export const routes: Routes = [
     { path: '', component: InicioComponent },
     { path: 'login', component: LogregComponent },
+    { path: 'verificar/:token', component: VerificacionComponent },
     { path: '**',   redirectTo: '', pathMatch: 'full' },
 ];
