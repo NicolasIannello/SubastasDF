b24471b621f221ec485a37e2fadeaa3e90558f40
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-13 16:34:52 -0300

    tabla users

--- src/app/componentes/panel-admin/usuarios/usuarios.component.ts
+++ src/app/componentes/panel-admin/usuarios/usuarios.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../../../servicios/admin.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
-export class UsuariosComponent {
+export class UsuariosComponent implements OnInit{
+  Usuarios:Array<any>=[];
+  
+  constructor(public api: AdminService){ }
+
+  ngOnInit(): void {
+    let dato={
+      'token':localStorage.getItem('token'),
+      'tipo': 1
+    }
+    this.api.cargarUsers(dato).subscribe({
+      next: (value)=>{
+        this.Usuarios = [...value.users, ...value.emps, ...value.viejos];                
+      },
+      error: (err)=>{
+      }
+    })
+  }
 
 }
---
+++ src/app/servicios/admin.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const base_url=environment.base_url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+  header:HttpHeaders;
+
+  constructor(private http: HttpClient) {
+    this.header=new HttpHeaders().set('Acces-Control-Allow-Origin','*');
+  }
+
+  cargarUsers(dato:any):Observable<any>{
+    return this.http.post(base_url+'/admin/users', dato, {'headers':this.header})
+  }
+  cargarUsersDesde(dato:any, url:string):Observable<any>{
+    return this.http.post(base_url+'/admin/users?desde='+url, dato, {'headers':this.header})
+  }
+}
