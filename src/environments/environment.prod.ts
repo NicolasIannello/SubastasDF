ca3395a1220e27b8a8d2997b2fc4258e847a1f2e
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-19 17:26:25 -0300

    back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //base_url:'https://vps-4154293-x.dattaweb.com/api'
-  base_url:'http://localhost:3000/api'
+  base_url:'https://vps-4154293-x.dattaweb.com/subasta'
+  //base_url:'http://localhost:3000/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //base_url:'https://vps-4154293-x.dattaweb.com/api'
-  base_url:'http://localhost:3000/api'
+  base_url:'https://vps-4154293-x.dattaweb.com/subasta'
+  //base_url:'http://localhost:3000/api'
 };
 
 /*
