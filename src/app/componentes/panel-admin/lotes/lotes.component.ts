b661089dc5497e2593f6e85a1ea70ce3f2a52dca
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-27 21:56:12 -0300

    vargar

--- src/app/componentes/panel-admin/lotes/lotes.component.ts
+++ src/app/componentes/panel-admin/lotes/lotes.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CrearLoteComponent } from "./crear-lote/crear-lote.component";
+import { AdminService } from '../../../servicios/admin.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-lotes',
@@ -9,12 +11,48 @@ import { CrearLoteComponent } from "./crear-lote/crear-lote.component";
   templateUrl: './lotes.component.html',
   styleUrl: '../usuarios/usuarios.component.css'
 })
-export class LotesComponent {
+export class LotesComponent implements OnInit{
   crear:boolean=false;
+  error:boolean=false;
+  Lotes:Array<any>=[];
+  total:number=0;
+  pagina:number=0;
+  ordenar:string="_id";
+  orden:string="1";
+
+  constructor(public api:AdminService) {}
+
+  ngOnInit(): void {
+    this.cargarLotes();
+  }
 
   handleMessage(message: boolean, tipo:string) {    
     switch (tipo) {
-      case 'crear': this.crear=message; break;
+      case 'crear': 
+        this.crear=message;
+        this.cargarLotes(); 
+      break;
     }
+  }
+
+  cargarLotes(){
+    let datos={
+      'token':localStorage.getItem('token'),
+      'tipo':1
+    }
+    this.api.cargarLotes(datos,this.pagina*20,this.ordenar,this.orden).subscribe({
+      next:(value)=> {
+          if(value.ok) {
+            this.Lotes=value.lotes;
+            this.total=value.total;
+          }else{
+            this.error=true;
+          }
+      },
+      error:(err)=> {
+        this.error=true;
+        Swal.fire({title:'Ocurrio un error', confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'});
+      },
+    })
   }
 }
--- src/app/servicios/admin.service.ts
+++ src/app/servicios/admin.service.ts
@@ -57,4 +57,7 @@ export class AdminService {
       return false;
     }
   }
+  cargarLotes(dato:any, url:number,order:string,orden:string):Observable<any>{
+    return this.http.post(base_url+'/lote/lotes?desde='+url+'&order='+order+'&orden='+orden, dato, {'headers':this.header})
+  }
 }
