731bb6af9b9e4f318f07559f042b2d36926352e1
NicolasIannello <nicolas.iannello99@gmail.com>
2024-09-28 16:44:33 -0300

    ver lote

--- src/app/componentes/panel-admin/lotes/lotes.component.ts
+++ src/app/componentes/panel-admin/lotes/lotes.component.ts
@@ -1,24 +1,28 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CrearLoteComponent } from "./crear-lote/crear-lote.component";
 import { AdminService } from '../../../servicios/admin.service';
 import Swal from 'sweetalert2';
+import { VerLoteComponent } from "./ver-lote/ver-lote.component";
 
 @Component({
   selector: 'app-lotes',
   standalone: true,
-  imports: [CommonModule, CrearLoteComponent],
+  imports: [CommonModule, CrearLoteComponent, VerLoteComponent],
   templateUrl: './lotes.component.html',
   styleUrl: '../usuarios/usuarios.component.css'
 })
 export class LotesComponent implements OnInit{
   crear:boolean=false;
+  ver:boolean=false;
   error:boolean=false;
   Lotes:Array<any>=[];
   total:number=0;
   pagina:number=0;
   ordenar:string="_id";
   orden:string="1";
+  loteModal:Array<any>=[];
+  @ViewChild(VerLoteComponent)verComp!:VerLoteComponent;
 
   constructor(public api:AdminService) {}
 
@@ -31,6 +35,10 @@ export class LotesComponent implements OnInit{
       case 'crear': 
         this.crear=message;
         this.cargarLotes(); 
+      break;
+      case 'ver': 
+        this.ver=message;
+        this.loteModal=[];
       break;
     }
   }
@@ -51,6 +59,24 @@ export class LotesComponent implements OnInit{
       },
       error:(err)=> {
         this.error=true;
+        Swal.fire({title:'Ocurrio un error', confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'});
+      },
+    })
+  }
+
+  verLote(id:string){
+    this.ver=!this.ver;
+    let datos={
+      'uuid':id,
+      'token':localStorage.getItem('token'),
+      'tipo':1
+    }
+    this.api.cargarLote(datos).subscribe({
+      next:(value)=> {
+        this.loteModal=value.lote[0];
+        this.verComp.cargarImagenes(value.lote[0].img, value.lote[0].pdf);
+      },
+      error:(err)=> {
         Swal.fire({title:'Ocurrio un error', confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'});
       },
     })
