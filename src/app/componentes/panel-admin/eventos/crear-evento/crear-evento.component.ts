c52c872c643d7e5415ca1b33c6a5bf394621f5de
NicolasIannello <nicolas.iannello99@gmail.com>
2024-10-09 13:39:02 -0300

    img evento

--- src/app/componentes/panel-admin/eventos/crear-evento/crear-evento.component.ts
+++ src/app/componentes/panel-admin/eventos/crear-evento/crear-evento.component.ts
@@ -14,7 +14,9 @@ import { CommonModule } from '@angular/common';
 export class CrearEventoComponent {
   @Output() messageEvent = new EventEmitter<boolean>();
   datos:Array<any>=     ['','Autos y motos','','','Remates',true,true];
-  alertas:Array<string>=['',''             ,'','',''       ,''];
+  alertas:Array<string>=['',''             ,'','',''       ,'',''];
+  img:any|null=[];
+  sources:any='';
 
   constructor(public api:AdminService) {}
 
@@ -30,6 +32,8 @@ export class CrearEventoComponent {
       if(this.datos[i]==='') flag=false;
       this.alertas[i]= (this.datos[i]==='') ? 'Campo obligatorio' : '';
     }
+    if(this.img==null || this.img.length==0) flag=false;
+    this.alertas[7]= (this.img==null || this.img.length==0) ? 'Campo obligatorio' : '';
 
     if(flag){
       let datos={
@@ -47,11 +51,26 @@ export class CrearEventoComponent {
       this.api.crearEvento(datos).subscribe({
         next:(value)=> {
           if(value.ok){
-            Swal.fire({title:'Evento creado con exito',confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'})
+            const formData = new FormData();
+            formData.append('uuid', value.uuid);  
+            formData.append('img', this.img[0]);  
+            formData.append('caso', 'nuevo');
+            formData.append('token', localStorage.getItem('token')!);  
+            formData.append('tipo', '1');	
+            this.api.imagenEvento(formData).then(resp =>{
+              if(resp.ok){
+                Swal.fire({title:'Evento creado con exito',confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'})
+              }else{
+                Swal.fire({title:'Error en la carga de imagen',confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'})
+              }
+              this.cerrarModal();
+            }, (err)=>{				
+              Swal.fire({title:'Error en la carga de imagen',confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'})
+              this.cerrarModal();   
+            });
           }else{
             Swal.fire({title:value.msg,confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'})
           }
-          this.cerrarModal();   
         },
         error:(err)=> {
           Swal.fire({title:'Ocurrio un error',confirmButtonText:'Aceptar',confirmButtonColor:'#3083dc'});
@@ -61,4 +80,17 @@ export class CrearEventoComponent {
     }
   }
 
+showImg(event: Event){
+		this.sources='';
+    this.img=null;
+    const element = event.currentTarget as HTMLInputElement;
+		this.img=element.files;    
+    
+    const reader = new FileReader();
+    reader.readAsDataURL(element.files![0]);
+
+    reader.onloadend = ()=>{
+      this.sources=reader.result;
+    }
+	}
 }
