47c5cc51f627f715d8254896d9ee0084baa499ef
NicolasIannello <nicolas.iannello99@gmail.com>
2024-10-09 21:44:04 -0300

    eventos cards

--- src/app/componentes/landing/inicio/inicio.component.ts
+++ src/app/componentes/landing/inicio/inicio.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AdminService } from '../../../servicios/admin.service';
+import { ServiciosService } from '../../../servicios/servicios.service';
 
 @Component({
   selector: 'app-inicio',
@@ -8,7 +10,38 @@ import { Component, Input } from '@angular/core';
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
-export class InicioComponent {
+export class InicioComponent implements OnInit{
   @Input() widthC: number | undefined;
   @Input() cap: number | undefined;
+  Eventos:Array<any>=[];
+  error:number=0
+
+  constructor(public api:AdminService, public api2:ServiciosService) {}
+
+  ngOnInit(): void {
+    let datos={
+      'dato': 'home',
+      'token':localStorage.getItem('token'),
+      'tipo':1
+    }      
+    
+    this.api.cargarEvento(datos).subscribe({
+      next:(value)=> {          
+        if(value.ok) this.Eventos=value.evento
+        if(!value.ok) this.error=1;
+        if(value.t && value.t==3) this.error=3
+        console.log(this.Eventos);
+        for (let i = 0; i < this.Eventos.length; i++) {
+          this.api2.cargarArchivo(this.Eventos[i].img.img,'evento').then(resp=>{						
+            if(resp!=false){
+              this.Eventos[i].img.img=resp.url;
+            }
+          })
+        }
+      },
+      error:(err)=> {
+        this.error= !err.error.ok ? 1 : 2
+      },
+    })  
+  }
 }
