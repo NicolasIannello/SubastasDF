acce9aa1c6ce84b6cb81cdf0a2060d28d572ed2d
NicolasIannello <nicolas.iannello99@gmail.com>
2024-10-23 14:27:18 -0300

    false go to /

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,13 +1,16 @@
-import { ActivatedRouteSnapshot, CanActivate, CanActivateFn, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { ServiciosService } from '../servicios/servicios.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class authGuard implements CanActivate {
-  constructor(private api: ServiciosService,) {}
+  constructor(private api: ServiciosService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): any {
-    if(!localStorage.getItem('token')) return false
+    if(!localStorage.getItem('token')) {
+      this.router.navigate(['/']);    
+      return false
+    }
     let datos={
       'token': localStorage.getItem('token'),
       'tipo': 3
@@ -17,6 +20,7 @@ export class authGuard implements CanActivate {
         return value.ok;
       },
       error: (err)=>{
+        this.router.navigate(['/']);
         return false;
       }
     })
